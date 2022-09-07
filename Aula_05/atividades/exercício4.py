e11267708cde36b479fd1a2299d7eadc4960d9c2
Talita Hanna Cabral <talitalinden@gmail.com>
2022-09-07 16:49:13 -0300

    exercício exemplo 4

---
+++ Aula_05/atividades/exercício4.py
@@ -0,0 +1,9 @@
+
+nome = "White"
+idade = 35
+signo = "Heisenberg"
+altura = 1.77
+
+print(Meu nome é {}, e tenho {} de idade. \
+    Sou do signo de {} e minha altura é de {} \
+        mt.format(nome, idade, signo, altura))
