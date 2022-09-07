5c158534e112552c0c481b3837cb8e801dc436b8
Talita Hanna Cabral <talitalinden@gmail.com>
2022-09-07 14:52:46 -0300

    primeiro envio da aula em laboratório

---
+++ Aula_02/atividades/aulamessias.py
@@ -0,0 +1,32 @@
+
+#mensagem = "Eu amo programar em Python"
+#print (mensagem)
+
+#nome = input("Digite o seu nome:")
+#print (f"Bom dia, {nome}.")
+
+
+#numero = float(input ("Digite um número: "))
+#print ('primeira forma --- todos os valores separados')
+#print (numero + 1357)
+#print(numero * 8)
+#print(numero / 5)
+#print(numero ** 2)
+
+#print("Segunda forma - Como uma expressão") 
+
+resultado = (((numero + 1357) * 8) / 5) ** 2
+
+print("O resultado desse calculo é", resultado)
+
+numero = int(input("Digite um número inteiro: "))
+
+#numero = input("Digite outro número: ")
+numero = numero + 1357
+print(numero)
+resultado = numero + 1357
+resultado = resultado * 8
+resultado = resultado / 5
+resultado = resultado ** 2
+
+#print("O resultado é {}".format(resultado))
---
+++ Aula_02/atividades/emsaladeaula.py
@@ -0,0 +1,5 @@
+mensagem = "Eu amo programar em Python"
+print (mensagem)
+
+nome = input("Digite o seu nome:")
+print (f"Bom dia, {nome}.")
