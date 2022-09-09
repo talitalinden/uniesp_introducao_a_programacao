714ec1d45852560fc526112109286e033ce4d7b0
Talita Hanna Cabral <talitalinden@gmail.com>
2022-09-09 17:38:24 -0300

    desvio condicional

---
+++ Aula_3/atividades/desviocondicional.py
@@ -0,0 +1,7 @@
+# Desvio condicional
+# As ações são exectadas da esquerda para a direita e o ; (ponto e vírgula) finaliza a linha de execução de uma ação.
+
+#somando idade
+idade = int(input("Digite a sua idade"))
+nova_idade = idade+5
+print(nova_idade)
