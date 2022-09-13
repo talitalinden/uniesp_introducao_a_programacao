eb1b62d8b5286949e125ea85b2dd60ea19d4d950
Talita Hanna Cabral <talitalinden@gmail.com>
2022-09-13 19:55:22 -0300

    sequência de comandos

--- Aula_06/atividades/exemolo2.py
+++ Aula_06/atividades/exemolo2.py
@@ -2,8 +2,10 @@
 #a função len retorna um número inteiro, ou seja, o tamanho da lista
 lista_de_nomes = ["Conrrado", "Angélica", "Zacarias", "Maria", "Berenice", "Miguel"]
 i = len(lista_de_nomes) - 1
-print(i)
 
 while i >= 0:
     print("Olá {}, você foi convidado para um jantar beneficente.".format(lista_de_nomes[i]))
     i = i - 1
+for num in range(5)
+print("O número agora é:" + stg(num))
+print(i)
