8304cc5375090650ce54070b88c7d0b9e2ce5351
Talita Hanna Cabral <talitalinden@gmail.com>
2022-09-13 20:53:31 -0300

    for num in range

--- Aula_06/atividades/forinrange.py
+++ Aula_06/atividades/forinrange.py
@@ -1,3 +1,9 @@
 for num in range(1000, 2001):
     if (num % 11) == 5:
         print(num)
+
+for num in range(1, 11):
+    print(f"Tabuada do {num}")
+    for num2 in range(1, 11):
+        resultado = num * num2
+        print(f'{num} x {num2} = {resultado}')
