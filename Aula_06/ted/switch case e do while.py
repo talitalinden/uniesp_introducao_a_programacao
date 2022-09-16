828664c629a4c47798ecb26cea54e3c19af5076d
Talita Hanna Cabral <talitalinden@gmail.com>
2022-09-16 09:12:59 -0300

    swuitch case e do while

---
+++ Aula_06/ted/switch case e do while.py
@@ -0,0 +1,14 @@
+#Switch/case é um comando que controla o fluxo do programa, permitindo ao programador  especificar código 
+# diferente para ser executado em várias condições. O switch compara o valor de uma variável aos valores especificados no comando case. 
+# no Javascript tem um valor condicional que serve para analisar os valores e executar um bloco de códigos.
+# em Python não possui o comando switch case, mas no seu lugar usa-se a estrutura if-else.
+
+#Do While - Esta instrução é usada quando não sabemos quantas vezes um determinado bloco de instruções precisa ser repetido. 
+#Com ele, a execução das instruções vai continuar até que uma condição seja verdadeira.
+# em Python, o comando While faz com que um conjunto de instruções seja executado enquanto uma condição é atendida.
+# Quando o resultado dessa condição passa a ser falso, a execução do loop é interrompida.
+
+contador = 0
+while (contador < 5):
+    print(contador)
+    contador = contador + 1
