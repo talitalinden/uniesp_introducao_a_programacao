564ef94f5c19658e9a6677d9e2b9be448e68d290
Talita Hanna Cabral <talitalinden@gmail.com>
2022-09-16 09:21:57 -0300

    do while com uso do else

--- Aula_06/ted/switch case e do while.py
+++ Aula_06/ted/switch case e do while.py
@@ -12,3 +12,12 @@ contador = 0
 while (contador < 5):
     print(contador)
     contador = contador + 1
+
+# Ao final do While pode-se utilizar a instrução else para executar alguma instrução ou bloco de códigos ao final do loop.
+
+contador = 0
+while (contador < 5):
+    print(contador)
+    contador = contador + 1
+else:
+    print("O loop while foi encerrado com sucesso")
